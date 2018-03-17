86cb2ed6569bfd26bed1a33ce21ffe17594009d9
Angeliki Beyko <abeyko@users.noreply.github.com>
2018-03-17 08:46:07 -0500

    implemented polling
    to switch between functions and save animation frame globally so it can check for button presses in between updating the leds

--- main.ino
+++ main.ino
@@ -1,130 +1,124 @@
-/*
- * H2Go 
-
- * Soft transluscent rubber raindrop with embedded electronics.
- * 
- * H2Go is fully portable and reminds users to drink 8 glasses of
- * water a day.
- * 
- * LED brightness changes from dim to fully bright
- * and then reverses from fully bright to dim. Does repeatedly until
- * user presses down on raindrop to indicate they have drank a 
- * glass of water. 
- * 
- * Created by Angeliki Beyko on March 08, 2018
- * 
- * Sources used: 
- * http://diarmuid.ie/blog/pwm-exponential-led-fading-on-arduino-or-other-platforms/
- * https://github.com/FastLED/FastLED/wiki/Basic-usage
- * https://github.com/FastLED/FastLED/wiki/Pixel-reference
- * https://www.rapidtables.com/convert/color/rgb-to-hsv.html
- * 
- */
-
+// import library
 #include <FastLED.h>
-#define NUM_LEDS 60
+
+// define constants
+#define NUM_LEDS 60 // CHANGE THIS?
 #define DATA_PIN 6
+#define BUTTON_PIN 2
+#define PWM_INTERVALS 100
+#define LED_UPDATE_INTERVAL 2000 // INSTEAD OF 20 
+#define BUTTON_INTERVAL 100 // INSTEAD OF 100
+#define ANIMATION_UPDATE_INTERVAL 1000 // INSTEAD OF 10
 
 CRGB leds[NUM_LEDS];
 
-unsigned long startTime;
-unsigned long endTime;
-
+int animation_frame = 0;
 int brightness = 0;
+int base = 10;
 
-// The number of Steps between the output being on and off
-const int pwmIntervals = 100;
+unsigned long ledUpdateInterval_previous = 0; 
+unsigned long buttonInterval_previous = 0; 
+unsigned long animationUpdateInterval_previous = 0;
 
 // The R value in the graph equation
 float R;
 
-void setup() { 
+void setup() {
   Serial.begin(9600);
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  
-  // set the pin connected to the LED as an output
-  pinMode(DATA_PIN, OUTPUT);
 
   // calculate the R variable (only needs to be done once at setup)
-  R = (pwmIntervals * log10(10))/(log10(255)); // increase numerator for smoother fade
+  R = (PWM_INTERVALS * log10(base))/(log10(255));
+  
+  pinMode(BUTTON_PIN, INPUT);
+
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  pinMode(DATA_PIN, OUTPUT);
 }
 
-void loop() { 
-  pulse();
+void loop() {
+  unsigned long currentMillis1 = millis();
+  int millisElapsed1 = currentMillis1 - ledUpdateInterval_previous;
+
+  if (millis() - animationUpdateInterval_previous >= ANIMATION_UPDATE_INTERVAL) {
+    pulse();
+//    animate_leds();
+    animationUpdateInterval_previous = millis();
+  }
+
+  if (millis() - ledUpdateInterval_previous >= LED_UPDATE_INTERVAL) {
+    
+    FastLED.show();
+    ledUpdateInterval_previous = millis();
+  }
+
+  if (millis() - buttonInterval_previous >= BUTTON_INTERVAL) {
+
+    int buttonState = digitalRead(BUTTON_PIN);
+    
+    Serial.print("button state is: ");
+    Serial.println(buttonState);
+
+    buttonInterval_previous = millis();
+  }
 }
 
 void set(int brightness) {
-  for(int dot = 0; dot < NUM_LEDS; dot++) { 
-    leds[dot].setHSV(0,0,brightness); // hue, sat, val or "brightness"
+  for(int dot = 0; dot < NUM_LEDS; dot++) {
+    if (dot <= animation_frame)  {
+      leds[dot].setHSV(0,0,brightness);
+    }
   }
-  FastLED.show(); // update only once after entire array is set
+
+  animation_frame++;
+  if (animation_frame > NUM_LEDS) {
+    animation_frame=0;
+  }
+
+  FastLED.show();
 }
 
-//void chase() {
-//  for(int dot = 0; dot < NUM_LEDS; dot++) { 
-//    leds[dot] = CRGB::White;
-//    FastLED.show();
-//    // clear this led for the next time around the loop
-//    leds[dot] = CRGB::Black;
-//    delay(30);
-//  }
-//}
-//
-//void on() {
-//   for (int dot = 0; dot < NUM_LEDS; dot++) {
-//     leds[dot] = CRGB::White; 
-//     FastLED.show(); 
-//     delay(30);   
-//  }
-//}
-
 void updateBrightness(int interval) {
-    // calculate the required PWM value for this interval step
-    brightness = pow(10, (interval / R)) - 1; // power input should match numerator above
+  // calculate the required PWM value for this interval step
+  brightness = pow(base, (interval / R)) - 1;
+  
+  // set the LED ring output to the calculated brightness
+  set(brightness);
 
-    // set the LED ring output to the calculated brightness
-    set(brightness);
-
-    // print the loop iteration and brightness levels
-    Serial.println(brightness);
-
-    // delay for 10 ms between each loop
-    delay(10);
+//  Serial.println(brightness);
+  delay(5); // WAS 10
 }
 
 void brighten() {
-  // LED ring brightness increasing
-  Serial.println("RAMPING UP......");
-
-  // loop through from 0 to 100, counting up
-  for (int interval = 0; interval <= pwmIntervals; interval++) {
+//  Serial.println("RAMPING UP......");
+  for (int interval = 0; interval <= PWM_INTERVALS; interval++) {
     updateBrightness(interval);
   }
 }
 
 void dim() {
-  // LED ring brightness decreasing
-  Serial.println("WINDING DOWN......");
-
-  // loop through from 100 to 0, counting down
+//  Serial.println("WINDING DOWN......");
   for (int interval = 100; interval >= 0; interval--) {
     updateBrightness(interval);
   }
 }
 
 void pulse() {
-  startTime = micros();
-  
   brighten();
   dim();
+}
 
-  endTime = micros();
-  unsigned long difference = endTime - startTime;
-  Serial.print("startTime is: ");
-  Serial.println(startTime);
-  Serial.print("endTime is: ");
-  Serial.println(endTime);
-  Serial.print("PULSE FUNCTION EXECUTION TIME: ");
-  Serial.print(difference / 1000); // converting microseconds to milliseconds
-  Serial.println(" ms");
+void animate_leds() {
+  for(int dot = 0; dot < NUM_LEDS; dot++) { 
+    if (dot <= animation_frame)  {
+      leds[dot] = CRGB::Blue;
+    }
+    else {
+      leds[dot] = CRGB::Black;
+    }
+  }
+
+  animation_frame++;
+  if (animation_frame > NUM_LEDS) {
+    animation_frame=0;
+  }
 }
